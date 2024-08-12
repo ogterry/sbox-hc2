06c88b69b2eb5dcb748a4d208ddc84a763bf3afe
Carson Kompon <carsokompo@gmail.com>
2024-08-12 12:50:53 -0400

    Created PlayerExperience Component and added to Player.

---
+++ Code/Player/PlayerExperience.cs
@@ -0,0 +1,16 @@
+using Sandbox;
+
+namespace HC2;
+
+public sealed class PlayerExperience : Component, ISaveData
+{
+	public string Save()
+	{
+		return "";
+	}
+
+	public void Load( string data )
+	{
+
+	}
+}
