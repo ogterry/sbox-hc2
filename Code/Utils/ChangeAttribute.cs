3a59b2d938def03eb05d14b4d6ad3cecbaaa2c76
Tony Ferguson <devultj@gmail.com>
2024-08-12 19:06:18 +0100

    Add ChangeAttribute

---
+++ Code/Utils/ChangeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HC2;
+
+[CodeGenerator( CodeGeneratorFlags.Instance | CodeGeneratorFlags.WrapPropertySet, "ChangeAttribute.OnPropertySet", 10 )]
+public class ChangeAttribute : Attribute
+{
+	public string Callback { get; set; }
+
+	public ChangeAttribute( string callbackName )
+	{
+		Callback = callbackName;
+	}
+	
+	internal static void OnPropertySet<T>( WrappedPropertySet<T> p )
+	{
+		var attribute = p.Attributes.OfType<ChangeAttribute>().FirstOrDefault();
+		var type = TypeLibrary.GetType( p.TypeName );
+		var method = type.GetMethod( attribute.Callback );
+		var property = TypeLibrary.GetMemberByIdent( p.MemberIdent ) as PropertyDescription;
+		var oldValue = property.GetValue( p.Object );
+		var isTheSame = p.Value.Equals( oldValue );
+		
+		p.Setter( p.Value );
+
+		if ( isTheSame )
+			return;
+		
+		try
+		{
+			method.Invoke( p.Object, new[] { oldValue, p.Value } );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e );
+		}
+	}
+}
