3291f0ef7b146a7094270820e3311ccf3b2d23cc
James King <james.king@facepunchstudios.com>
2024-08-13 14:58:02 +0200

    Add ResourceGatherer weapon component

---
+++ Code/Player/Weapons/ResourceGatherer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC2;
+
+public enum GatherSourceKind
+{
+	None,
+	Wood,
+	Stone,
+	Metal
+}
+
+public sealed class ResourceGatherer : WeaponComponent
+{
+	[Property] public Dictionary<GatherSourceKind, float> Effectiveness { get; set; } = new();
+}
