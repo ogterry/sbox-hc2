94d620156bf0f71c2da89b857b193dbad0941869
James King <james.king@facepunchstudios.com>
2024-08-13 15:50:04 +0200

    Placeholder resource node

--- Code/Gameplay/ResourceNode.cs
+++ Code/Gameplay/ResourceNode.cs
@@ -1,4 +1,5 @@
-﻿using Sandbox.Events;
+﻿using System;
+using Sandbox.Events;
 
 namespace HC2;
 
@@ -29,6 +30,11 @@ public sealed class ResourceNode : Component,
 	private float _spareDamage;
 	private TimeSince _sinceDepleted;
 
+	protected override void OnStart()
+	{
+		UpdateDepletion( 1f );
+	}
+
 	private float GetWeaponEffectiveness( DamageInstance damageInfo )
 	{
 		Log.Info( damageInfo.Inflictor );
@@ -44,6 +50,12 @@ public sealed class ResourceNode : Component,
 
 	void IGameEventHandler<ModifyDamageEvent>.OnGameEvent( ModifyDamageEvent eventArgs )
 	{
+		if ( Health.Health <= 0f )
+		{
+			eventArgs.ClearDamage();
+			return;
+		}
+
 		var effectiveness = GetWeaponEffectiveness( eventArgs.DamageInstance );
 
 		eventArgs.ScaleDamage( effectiveness );
@@ -63,13 +75,17 @@ public sealed class ResourceNode : Component,
 			_spareDamage -= DamagePerItem;
 			DropItem( (eventArgs.Instance.Position - Transform.Position).Normal );
 		}
+
+		var health = Health.Health - eventArgs.Instance.Damage;
+
+		UpdateDepletion( Math.Clamp( health / Health.MaxHealth, 0f, 1f ));
 	}
 
 	private void DropItem( Vector3 direction )
 	{
 		Log.Info( "Dropped!" );
 
-		// Just find closest player
+		// TODO: drop in world
 
 		var player = Scene.GetAllComponents<Player>()
 			.MinBy( x => x.Transform.Position - Transform.Position );
@@ -85,5 +101,36 @@ public sealed class ResourceNode : Component,
 	void IGameEventHandler<KilledEvent>.OnGameEvent( KilledEvent eventArgs )
 	{
 		_sinceDepleted = 0f;
+	}
+
+	[Broadcast( NetPermission.HostOnly )]
+	private void UpdateDepletion( float fraction )
+	{
+		var found = false;
+		var stages = Components.GetAll<ResourceDepletionStage>( FindMode.EverythingInChildren )
+			.OrderBy( x => x.Fraction );
+
+		foreach ( var stage in stages )
+		{
+			if ( !found && stage.Fraction >= fraction )
+			{
+				found = true;
+				stage.GameObject.Enabled = true;
+			}
+			else
+			{
+				stage.GameObject.Enabled = false;
+			}
+		}
+	}
+}
+
+public sealed class ResourceDepletionStage : Component
+{
+	[Property, Range( 0f, 1f )] public float Fraction { get; set; } = 1f;
+
+	protected override void OnValidate()
+	{
+		GameObject.Name = $"{Fraction * 100f:F0}%";
 	}
 }
