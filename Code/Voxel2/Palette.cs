17022a4bf632535b1e6c5c9a5e37495e83f08e7c
kurozael <kurozael@gmail.com>
2024-08-14 23:52:49 +0100

    Added [KeyProperty] Name to Palette entries

--- Code/Voxel2/Palette.cs
+++ Code/Voxel2/Palette.cs
@@ -7,6 +7,14 @@ public partial class Palette : GameResource
 {
 	public struct Material
 	{
+		public Material()
+		{
+			Name = "Unnamed";
+			Color = default;
+			Texture = null;
+		}
+
+		[KeyProperty] public string Name { get; set; }
 		public Color Color { get; set; }
 		public Texture Texture { get; set; }
 	}
