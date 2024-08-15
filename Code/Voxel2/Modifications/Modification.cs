dc60b42db151d4858e03bd489755ff2a57a54a7e
James King <james.king@facepunchstudios.com>
2024-08-15 20:01:46 +0200

    Add some documentation to IModification

--- Code/Voxel2/Modifications/Modification.cs
+++ Code/Voxel2/Modifications/Modification.cs
@@ -11,6 +11,15 @@ public enum ModificationKind : byte
 	Build
 }
 
+/// <summary>
+/// Interface for networked modifications. To create a new one:
+///
+/// <list type="number">
+/// <item>Add an entry to <see cref="ModificationKind"/></item
+/// <item>Return that in your <see cref="Kind"/> implementation</item>
+/// <item>Add a case in <see cref="VoxelNetworking.Apply(VoxelNetworking.Modification)"/></item>
+/// </list>
+/// </summary>
 public interface IModification
 {
 	ModificationKind Kind { get; }
@@ -18,6 +27,9 @@ public interface IModification
 	Vector3Int Min { get; }
 	Vector3Int Max { get; }
 
+	/// <summary>
+	/// If a chunk that's within Min and Max doesn't exist, should we create it?
+	/// </summary>
 	bool ShouldCreateChunk( Vector3Int chunkMin );
 
 	void Write( ref ByteStream stream );
